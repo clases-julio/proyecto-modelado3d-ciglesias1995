66ab233e084d8a5144f889510b0e730962798b13
Carlos <c.iglesias.2018@alumnos.urjc.es>
2022-03-16 23:35:57 +0100

    entrega

--- codigo.py
+++ codigo.py
@@ -93,11 +93,10 @@ class Objeto:
 ''' M  A  I  N '''
 '''************'''
 if __name__ == "__main__":
-    # Creación de un cubo y transformaciones de este:
+
     borrarObjetos()
-    #bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(0, 0, 0), rotation=(3.14, 0, 0), scale=(1, 1, 1))
-    #bpy.ops.view3d.camera_to_view()
     
+    # Creamos el cuerpo del OVNI
     bpy.ops.mesh.primitive_cone_add(radius1=1, radius2=0, depth=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(8, 8, 1))
     Activo.renombrar("main_OVNI_body")
     bpy.ops.mesh.primitive_cone_add(radius1=1, radius2=0, depth=2, enter_editmode=False, align='WORLD', location=(0, 0, 0.832209), scale=(4, 4, 1))
@@ -107,10 +106,9 @@ if __name__ == "__main__":
     Activo.renombrar("OVNI_windshield")
     seleccionarObjeto("main_OVNI_body")
     
-    # Join both OVNI bodies
+    # Unimos las dos partes
     body = []
     body.append("upper_OVNI_body")
-    #body.append("main_OVNI_body")
     body.append("OVNI_windshield")
     seleccionarVariosObjetos(body)
     bpy.ops.object.join()
@@ -121,8 +119,6 @@ if __name__ == "__main__":
     bpy.ops.mesh.primitive_cylinder_add(end_fill_type='NOTHING', enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
     bpy.ops.object.modifier_add(type='SOLIDIFY')
     bpy.context.object.modifiers["Solidify"].thickness = 0.09 # grosor 0.09
-
-
     Activo.renombrar("main_motor_body")
     Seleccionado.rotarX(1.57079)
   
@@ -146,7 +142,6 @@ if __name__ == "__main__":
     bpy.ops.mesh.primitive_cone_add(radius1=1, radius2=0, depth=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
     Activo.renombrar("reactor")
     bpy.ops.transform.translate(value=(0, 1.02379, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, True, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
-
     bpy.context.object.rotation_euler[0] = -1.5708
     bpy.context.object.scale[0] = 0.5
     bpy.context.object.scale[1] = 0.5
@@ -154,38 +149,32 @@ if __name__ == "__main__":
 
     # Truncamos el cono 
     bpy.ops.object.editmode_toggle()
-
     bpy.ops.mesh.bevel(offset=1.08926, offset_pct=0, affect='VERTICES')
     bpy.ops.object.editmode_toggle()
-
-    bpy.ops.mesh.primitive_cylinder_add(end_fill_type='NOTHING', enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(0.5, 0.5, 0.1))
     
+    # Añadimos cilindro hueco al final del reactor
+    bpy.ops.mesh.primitive_cylinder_add(end_fill_type='NOTHING', enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(0.5, 0.5, 0.1))
     bpy.ops.object.modifier_add(type='SOLIDIFY')
     Seleccionado.rotarX(1.57079)
     bpy.context.object.modifiers["Solidify"].thickness = 0.09 # grosor 0.09
     bpy.ops.transform.translate(value=(-0, -1.06975, -0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
     Activo.renombrar("output_reactor")
+    
+    # Unimos ambas piezas
     motor_tot = []
     motor_tot.append("reactor")
-    #motor_tot.append("motor_tape")
     motor_tot.append("output_reactor")
     seleccionarVariosObjetos(motor_tot)
  
     bpy.ops.object.join()
     seleccionarObjeto("output_reactor")
+    
+    #Lo colocamos dentro del cubo
     bpy.ops.transform.translate(value=(-7.79881, -1.09452, -0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
     
 
 
-
-
-
-
-
-
-
-
-    # Definimos primer cilidro hueco del motor
+    # Definimos primer cilidro hueco del segundo motor
     bpy.ops.mesh.primitive_cylinder_add(end_fill_type='NOTHING', enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
     bpy.ops.object.modifier_add(type='SOLIDIFY')
     bpy.context.object.modifiers["Solidify"].thickness = 0.09 # grosor 0.09
@@ -214,7 +203,6 @@ if __name__ == "__main__":
     bpy.ops.mesh.primitive_cone_add(radius1=1, radius2=0, depth=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
     Activo.renombrar("reactor2")
     bpy.ops.transform.translate(value=(0, 1.02379, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, True, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
-
     bpy.context.object.rotation_euler[0] = -1.5708
     bpy.context.object.scale[0] = 0.5
     bpy.context.object.scale[1] = 0.5
@@ -222,24 +210,24 @@ if __name__ == "__main__":
 
     # Truncamos el cono 
     bpy.ops.object.editmode_toggle()
-
     bpy.ops.mesh.bevel(offset=1.08926, offset_pct=0, affect='VERTICES')
     bpy.ops.object.editmode_toggle()
 
+    # Añadimos cilindro hueco al final del reactor
     bpy.ops.mesh.primitive_cylinder_add(end_fill_type='NOTHING', enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(0.5, 0.5, 0.1))
-    
     bpy.ops.object.modifier_add(type='SOLIDIFY')
     Seleccionado.rotarX(1.57079)
     bpy.context.object.modifiers["Solidify"].thickness = 0.09 # grosor 0.09
     bpy.ops.transform.translate(value=(-0, -1.06975, -0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
     Activo.renombrar("output_reactor2")
+    
+    # Unimos ambas piezas
     motor_tot = []
     motor_tot.append("reactor2")
-    #motor_tot.append("motor_tape")
     motor_tot.append("output_reactor2")
     seleccionarVariosObjetos(motor_tot)
- 
     bpy.ops.object.join()
     seleccionarObjeto("output_reactor2")
+    
+    #Lo colocamos dentro del otro cubo
     bpy.ops.transform.translate(value=(7.79881, -1.09452, -0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
-
